d537baa55a59f1a4295f4869eda4f9eabf23711d
trifyllalex <trifyllalex@dias.com.gr>
2012-10-08 14:29:50 +0300

    First Commit

---
+++ tddfirstproject/src/main/java/gr/alx/tddfirstproject/Template.java
@@ -0,0 +1,20 @@
+package gr.alx.tddfirstproject;
+
+public class Template {
+
+	private String variableValue;
+	private String templateText;
+
+	public Template(String templateText) {
+		this.templateText = templateText;
+	}
+
+	public void set(String variable, String value) {
+		this.variableValue = value;
+	}
+
+	public String evaluate() {
+		return templateText.replaceAll("\\$\\{name\\}", variableValue);
+	}
+
+}
---
+++ tddfirstproject/src/test/java/gr/alx/tddfirstproject/TestTemplate.java
@@ -0,0 +1,39 @@
+package gr.alx.tddfirstproject;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTemplate {
+
+	@Test
+	public void oneVariable() throws Exception {
+		Template template = new Template("Hello, ${name}");
+		template.set("name", "Reader");
+		assertEquals("Hello, Reader", template.evaluate());
+
+	}
+
+	@Test
+	public void differentValue() throws Exception {
+		Template template = new Template("Hello, ${name}");
+		template.set("name", "someone else");
+		assertEquals("Hello, someone else", template.evaluate());
+	}
+
+	@Test
+	public void differentTemplate() throws Exception {
+		Template template = new Template("Hi, ${name}");
+		template.set("name", "someone else");
+		assertEquals("Hi, someone else", template.evaluate());
+	}
+
+	@Test
+	public void multipleVariables() throws Exception {
+		Template template = new Template("${one}, ${two}, ${three}");
+		template.set("one", "1");
+		template.set("two", "2");
+		template.set("three", "3");
+		assertEquals("1, 2, 3", template.evaluate());
+	}
+}
